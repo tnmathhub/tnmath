5a382e4dbde2b7e39698d8038d04a275c54a9be2
tnmathhub <tnmathhub@gmail.com>
2026-08-13 15:34:43 +0530

    Delete backend directory

--- backend/tn/hub/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import StudentRegisterView, TeacherRegisterView
-
-urlpatterns = [
-    path("register/student/", StudentRegisterView.as_view(), name="register-student"),
-    path("register/teacher/", TeacherRegisterView.as_view(), name="register-teacher"),
-]
--- backend/tn/tn/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework.authtoken import views as auth_token_views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/token-auth/", auth_token_views.obtain_auth_token),
-    path("api/hub/", include("hub.urls")),
-]
